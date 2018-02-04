cd5bcb142353704004819e41044075c5d68a991b
Glitch (imgsearc) <none>
2018-02-04 11:54:05 +0000

    index view

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+var hbs  = require('express-handlebars')
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.engine('handlebars', hbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.use(express.static('public'))
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
+app.get("/", function (req, res) {
+  res.render('index')
+})
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.listen(process.env.PORT, () => console.log('listening..'))
